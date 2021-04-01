b3771e20dc008849730c45a29c3f9f0a698ce0ae
Iyxan23 <nurihsanalghifari@gmail.com>
2021-04-01 21:15:21 +0700

    add: Add a nice looking help

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import discord
 from discord.ext import commands
+from pretty_help import PrettyHelp # Because I'm lazy
 import os
 import datetime
 
-client = commands.Bot(command_prefix="+")
+client = commands.Bot(command_prefix="+", help_command=PrettyHelp())
 token = os.getenv("DISCORD_BOT_TOKEN")
 
 
