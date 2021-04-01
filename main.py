c644f1a9f0e223afaf61ff8503206a22e5edd9ed
Iyxan23 <nurihsanalghifari@gmail.com>
2021-04-01 19:40:20 +0700

    init: Initalize bot

---
+++ main.py
@@ -0,0 +1,54 @@
+import discord
+from discord.ext import commands
+import os
+import datetime
+
+client = commands.Bot(command_prefix="+")
+token = os.getenv("DISCORD_BOT_TOKEN")
+
+@client.event
+async def on_ready():
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("me por modir"))
+    print("Discord bot ready")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"🏓 Pong, that took {int(client.latency * 1000)}ms")
+
+@client.command()
+async def pong(ctx):
+    await ctx.send(f"🏓 Ping, that took {int(client.latency * 1000)}ms"[::-1])
+
+@client.command(name="whoami")
+async def whoami(ctx):
+    await ctx.send(f"You're {ctx.message.author.name}, dum dum")
+
+# @client.command()
+# async def purge(ctx, amount=3) :
+#     await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def idea(ctx, idea_for="server", idea=None):
+    channel = None
+
+    if idea_for == "server":
+        channel = client.get_channel(826514832005136465)
+
+    elif idea_for == "app":
+        channel = client.get_channel(790687893701918730)
+
+    else:
+        await ctx.send("Hey, the 2nd parameter can only be \"app\" (suggest something for the mod) or \"server\" (suggest something for the server).")
+        return
+
+    if idea == None:
+        await ctx.send("Hey, can you put your idea on the 3rd argument?")
+        return
+
+    embed = discord.Embed(description="**Idea:** " + idea, color=0x1891fb)
+    embed.set_author(name=ctx.message.author.display_name, icon_url=ctx.message.author.avatar_url)
+    embed.timestamp = datetime.datetime.utcnow()
+    embed.set_footer(text="SWBot")
+    await channel.send(embed=embed)
+
+client.run(token)
