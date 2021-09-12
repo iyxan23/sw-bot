3a06a997426bafe9b5083e61aa07b789e214bb19
Iyxan23 <nurihsanalghifari@gmail.com>
2021-09-12 20:42:40 +0700

    feat: implement simple ping command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+use std::env;
+use std::time::Instant;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("+"))
+        .group(&GENERAL_GROUP);
+
+    let token = env::var("DISCORD_BOT_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    let start = Instant::now();
+    let mut msg = msg.reply(ctx, "Pong! :ping_pong:").await?;
+
+    let duration = start.elapsed();
+
+    msg.edit(
+        ctx,
+        |edit_msg|
+            edit_msg.content(
+                format!("Pong! :ping_pong: That took {}ms", (duration.as_millis()))
+            )
+    ).await?;
+
+    Ok(())
 }
